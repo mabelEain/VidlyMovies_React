1a7d402b6b4bc37565312232f4092e1e6990dacc
mabelEain <mabeleain90@gmail.com>
2022-12-16 12:33:51 +0630

    add pagination on movies list and make current page active

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import Like from './common/like';
+import Pagination from './common/pagination';
 
 class Movies extends Component {
     state = { 
-        movies: getMovies()
+        movies: getMovies(),
+        currentPage: 1,
+        pageSize: 4
      }
      
     handleDelete = (movie) => {
@@ -20,8 +23,13 @@ class Movies extends Component {
         this.setState({ movies });
     };
 
+    handlePageChange = (page) => {
+        this.setState({ currentPage: page });
+    };
+
     render() {
         const { length: count } = this.state.movies;
+        const { pageSize, currentPage } = this.state;
 
         if (count === 0 )
             return <p>There are no movies in database!</p>;
@@ -55,6 +63,11 @@ class Movies extends Component {
                 ))}   
             </tbody>
         </table>
+        <Pagination
+            itemsCount={count} 
+            pageSize={pageSize}
+            currentPage={currentPage} 
+            onPageChange={this.handlePageChange}/>
         </React.Fragment>
         
     )}
